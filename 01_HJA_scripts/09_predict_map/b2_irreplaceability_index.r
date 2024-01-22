92aef82516c34afd73102348aef8bd2922cd3d43
Christian Devenish <c.devenish@keele.ac.uk>
2024-01-22 18:14:30 +0000

    irr analysis species prediction maps

--- 01_HJA_scripts/09_predict_map/b2_irreplaceability_index.r
+++ 01_HJA_scripts/09_predict_map/b2_irreplaceability_index.r
@@ -1,5 +1,6 @@
 ## Irreplaceability indices
 
+rm(list=ls())
 
 # HJA_analyses_Kelpie_clean # is the root folder and must have a .Rproj file in it for here::here() to work.
 # setwd() # set here to HJA_analyses_Kelpie_clean or use here::here()
@@ -8,7 +9,7 @@
 # ..... setup
 here()
 	
-pacman::p_load('dplyr', 'rgdal', 'raster','here','glue','corrplot','sjsdm','ggplot2')
+pacman::p_load('dplyr', 'terra','here','glue','corrplot','sjSDM','ggplot2')
 	
 source(here('01_HJA_scripts', '09_predict_map', 'source', 'irrAB.r'))
 source(here('01_HJA_scripts', '08_sjsdm','source', 'corvif-source.r'))
@@ -21,13 +22,11 @@ packageVersion('sjSDM')
 utm10N = 32610
 	
 period = "S1"
-date.model.run = '2023'			# check!!!
+date.model.run = '2024'			# check!!!
 abund = 'pa'		
 varsName = 'vars11'		
 minocc = 6
 cv = '5CV'		
-	
-outputpath = here('..','..','04_Output')
 	
 sjsdmV = '1.0.5'		# check!!!
 sjsdmVfolder = glue('sjsdm-{sjsdmV}')
@@ -35,65 +34,33 @@ sjsdmVfolder = glue('sjsdm-{sjsdmV}')
 sppdatapath = here('03_format_data','otu')
 gispath = here('03_format_data','gis')
 	
-resFolder = file.path("04_Output", "sjsdm_general_outputs", glue('{varsName}_{date.model.run}'))	
-irreFolder = file.path("04_Output", "prediction_map")
+resFolder = here::here("04_Output", "sjsdm_general_outputs", glue('{varsName}_{date.model.run}'))	
+irreFolder = here::here("04_Output", "prediction_map")
 predFolder = here::here('04_Output', "sjsdm_prediction_outputs", glue::glue('{varsName}_{date.model.run}'))
 	
 # ..... load-data
-# load raster templates - reduced areas
-load(file.path(gispath, "templateRaster.rdata")) ## r.msk, indNA aoi.pred.sf, r.aoi.pred - reduced area for plotting
-# plot(r.msk)
-	
-# load model result - for species classification
-model.train = readRDS(here(resFolder, glue('s-jSDM_tuned.model_{period}_{abund}_{cv}_min{minocc}_{varsName}_AUC_{date.model.run}.RDS')))
-	
-# load data for the model ........
-load(here(sppdatapath, glue('forbestm_data_{period}_random_min{minocc}_{date.model.run}_{varsName}.rdata')))
-	
-# !!! need to change here
-if (abund == 'pa')
-{
-	otu.train = otu.pa.train
-	otu.test = otu.pa.test
-	dim(otu.test)
-}
-	
-# str(otu.train) 
-#names(scale.env.test); formula.env
-	
-s.otu.train = as.matrix(otu.train)	
-attr(s.otu.train, 'dimnames') = NULL
-str(s.otu.train)
-	
-s.otu.test = as.matrix(otu.test)		
-attr(s.otu.test, 'dimnames') = NULL
-str(s.otu.test)
-	
-# otu.pa.csv, otu.qp.csv
 
-## load species AUC resutls for filtering
-load(file.path(predFolder, 'rdata', "sp_test_results.rdata")) # # sp.res.test, sp.res.train	
+## load species AUC results for filtering
+load(file.path(resFolder, "spp_test_data.rdata")) #auc_by_spp, rsq_final, tune.results
 
-
-
-# ..... irreplace-ana
+# ..... irreplace-analysis
 ########### Filter species for analysis ##########
-## Mean AUC per species (and other eval metrics)
-str(sp.res.test)
-	
-table(is.na(sp.res.test$auc))
-	
+
+## Mean AUC per species (and other eval metrics) from 5CV
+str(auc_by_spp)
+head(auc_by_spp) # contains OTU name, mean (AUC) and incidence
+
 ## Filter species by auc
 auc.filt = 0.70
-table(sp.res.test$auc > auc.filt)
+# threshold for presence absence data
+
+table(auc_by_spp$mean >= auc.filt)
 	
 # ## extract species over AUC filter
-# incidence 
-incidence = colSums(otu.train)/nrow(otu.train)
-	
+
 # get species names, BOLD ID etc
-spp = data.frame(species = colnames(otu.train)) %>%
-  tidyr::separate(col = species, into = c("OTU", "empty", "class", "order", "family",
+spp = auc_by_spp %>%
+  tidyr::separate(col = OTU, into = c("OTU", "empty", "class", "order", "family",
                                           "genus", "epithet", "BOLD", "BOLDID",
                                           "size"),
                   remove = FALSE, sep = "_", convert = TRUE) %>%  ## creates real NAs with convert = T
@@ -101,34 +68,33 @@ spp = data.frame(species = colnames(otu.train)) %>%
                                is.na(epithet) & is.na(genus) & is.na(family) ~ order,
                                is.na(epithet) & is.na(genus) ~ family,
                                is.na(epithet) ~ genus,
-                               TRUE ~ paste(genus, epithet, sep = "_")
-  )) %>%
-  dplyr::select(-empty)%>%
-  mutate(auc = sp.res.test$auc,
-         incidence = incidence,
-         best.name = paste(best.name, BOLDID, sep = "_"))
+                               TRUE ~ paste(genus, epithet, sep = "_"))
+         ,best.name = paste(best.name, BOLDID, sep = "_")
+         ) %>%
+  dplyr::select(-empty)
 	
 head(spp)
 	
 sum(is.na(spp$best.name))
 sum(grepl("NA_NA", spp$best.name))
 head(spp, 30)
+head(spp$best.name)
 	
 sum(is.na(spp$family))
 	
 
 ### Load prediction results
 # clamp predictions
-load(file.path(irreFolder, 'rdata', paste0("sjSDM_predictions_", "M1S1_", "min", minocc, "_", varsName, "_", abund, "_clamp", ".rdata")))
-# pred.mn.cl, pred.sd.cl
-	
+# load clamp predictions (working folder - too large for github)
+load(file.path("./working", paste0("sjSDM_predictions_", "M1S1_", "min", minocc, "_", varsName, "_", date.model.run, "_", abund, "_clamp", ".rdata")))
+# pred.mn.cl, pred.sd.cl	
 dim(pred.mn.cl)
 	
-pred.in.cl = pred.mn.cl[,sp.res.test$auc > auc.filt & !is.na(sp.res.test$auc)]
+pred.in.cl = pred.mn.cl[,auc_by_spp$mean >= auc.filt & !is.na(auc_by_spp$mean)]
 dim(pred.in.cl)
 	
 ## get species names 
-spp.in = spp[sp.res.test$auc > auc.filt & !is.na(sp.res.test$auc), ]
+spp.in <- spp[auc_by_spp$mean >= auc.filt & !is.na(auc_by_spp$mean), ]
 head(spp.in)
 	
 
@@ -139,12 +105,27 @@ str(beta.r.prob.res)
 # beta.r.prob_ct <- irrAB(x = x_in_ct, pc = 0.90, type = "total", r = rBlock)
 # beta.r.prob_ct.res <- irrAB(x = x_in_ct, pc = 0.90, type = "total")
 
-# save(beta.r.prob.res, file = file.path(irreFolder, 'rdata', glue("beta_res_{pc}_noagg.rdata")))
-	
+# save copy of raster
+save(beta.r.prob.res, file = file.path("working", glue("beta_res_{pc}_{varsName}_{date.model.run}_noagg.rdata")))
+
+## load raster templates
+gis_out = here('03_format_data','gis')
+load(file.path(gis_out, "templateRaster.rdata")) ## r.msk, indNA aoi.pred.sf, r.aoi.pred - reduced area for plotting
+
+# convert to (new) terra package format
+r.msk
+r.msk <- terra::rast(r.msk)	
+
 beta.pix <- r.msk
 beta.pix[indNA] <- beta.r.prob.res$beta
 
-writeRaster(beta.pix, file = file.path(gispath, "processed_gis_data", "r_utm", "beta_r_prob_noagg.tif"), overwrite = TRUE)
+plot(beta.pix)
+
+terra::writeRaster(beta.pix, file = file.path(predFolder, paste0("beta_r_prob_noagg_", varsName, "_", date.model.run ,".tif")), overwrite = TRUE)
+
+
+save(spp, spp.in, file = file.path(resFolder, "spp_test_data_names.rdata"))
+
 
 # png(filename = file.path("04_Output/figures", "irreplaceability_pc50.png"), units = "mm", height =200, width = 300, res = 200)
 # plot(beta.pix)
